d9f0c3abf2c923ef39457f194b0379a0fd9349ce
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-04-09 18:22:45 +0800

    feat: add <ScrollToTop /> component

--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -1,14 +1,18 @@
-// components
 import { Outlet } from 'react-router-dom';
+
+import ScrollToTop from '../../components/ScrollToTop';
 
 // style
 import s from './index.module.scss';
 
 const BasicLayout = () => {
   return (
-    <main className={s['basic-layout-container']}>
-      <Outlet />
-    </main>
+    <>
+      <ScrollToTop />
+      <main className={s['basic-layout-container']}>
+        <Outlet />
+      </main>
+    </>
   );
 };
 
