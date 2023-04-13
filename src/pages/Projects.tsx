04de4d73642c9a4cd078aaa0803b38fae0130a16
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-04-14 01:05:22 +0800

    feat: use query as filter type

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,14 +1,24 @@
+import { useSearchParams } from 'react-router-dom';
 // components
 import ProjectsPageLayout from '../layouts/ProjectsPageLayout';
 import ProjectList from '../components/ProjectList';
 import ProjectNavbar from '../components/ProjectNavbar';
 import HireMeMessage from '../components/HireMeMessage';
 
+// constants
+import PROJECT_LIST from '../constants/project-list.constant';
+
 const Projects = () => {
+  const [search] = useSearchParams();
+  const currentCategoryParam = search.get('category') ?? 'frontend-mentor';
+  const currentProjectList = PROJECT_LIST.filter(
+    (project) => project.category === currentCategoryParam,
+  );
+
   return (
     <ProjectsPageLayout>
       <ProjectNavbar />
-      <ProjectList />
+      <ProjectList projectList={currentProjectList} />
       <HireMeMessage />
     </ProjectsPageLayout>
   );
