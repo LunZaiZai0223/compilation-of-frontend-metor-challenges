5539d154935649d45ac81a872c6f06b6e541a8d0
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-04-14 01:03:57 +0800

    feat: add navbar

--- src/components/ProjectNavbar/index.tsx
+++ src/components/ProjectNavbar/index.tsx
@@ -1,26 +1,73 @@
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 // components
 import FadeAndMove from '../../UI/FadeAndMove';
 
 // plugins
-import { AiFillHome } from 'react-icons/ai';
+import { AiFillHome, AiFillProject, AiOutlineProject } from 'react-icons/ai';
 
 // style
 import s from './index.module.scss';
 
+interface SubNavbarItem {
+  subTitle: string;
+  searchParams: string;
+  icon: React.ReactElement;
+}
+
+const subNavbarList: SubNavbarItem[] = [
+  {
+    subTitle: 'Frontend Mentor',
+    searchParams: '?category=frontend-mentor',
+    icon: <AiOutlineProject />,
+  },
+  {
+    subTitle: 'Others',
+    searchParams: '?category=others',
+    icon: <AiFillProject />,
+  },
+];
+
+const getNavTitle = (query: string | null) => {
+  switch (query) {
+    case 'frontend-mentor':
+      return 'CHALLENGES';
+
+    case 'others':
+      return 'PROJECTS';
+
+    default:
+      return 'CHALLENGES';
+  }
+};
+
 const ProjectNavbar = () => {
+  const [search] = useSearchParams();
+  const navTitle = getNavTitle(search.get('category'));
+
   return (
     <FadeAndMove direction={'horizontal'} timeout={2000} moveDistanceType={'sm'}>
       <div>
         <nav className={s.navbar}>
-          <h2>CHALLENGES</h2>
+          <h2>{navTitle}</h2>
         </nav>
-        <div className={s['sub-navbar']}>
-          <Link to='../' className={s.action}>
-            <AiFillHome /> HOME
-          </Link>
-        </div>
+        <ul className={s['sub-navbar']}>
+          <li>
+            <Link to='../' className={s.action}>
+              <AiFillHome /> HOME
+            </Link>
+          </li>
+          {subNavbarList.map((subNavbar, index) => {
+            const { subTitle, icon, searchParams } = subNavbar;
+            return (
+              <li key={index}>
+                <Link relative='path' to={{ search: searchParams }} className={s.action}>
+                  {icon} {subTitle}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </FadeAndMove>
   );
