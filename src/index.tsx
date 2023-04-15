3e57ff37917a11a1ee520f160cef3ee77974dae8
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-04-15 17:13:30 +0800

    feat: configure i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
 import reportWebVitals from './reportWebVitals';
 import router from './router';
+import i18n from '../src/lang/i18n';
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<RouterProvider router={router} />);
+root.render(
+  <I18nextProvider i18n={i18n}>
+    <RouterProvider router={router} />
+  </I18nextProvider>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
